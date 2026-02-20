c7198db7fa13cf8347e5d2195121440df00d3de3
pradyumn8 <pradyumn8@gmail.com>
2026-02-20 16:28:08 +0530

    fixed missing css

--- JS/script.js
+++ JS/script.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         </div>
                         <div class="hero-highlight-card">
                             <span class="highlight-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg></span>
-                            <span class="highlight-text"><span class="hero-counter" data-target="96">0</span>–<span class="hero-counter" data-target="99">0</span>% Methane Purity</span>
+                            <span class="highlight-text" style="font-size: 1.4rem;"><span class="hero-counter" data-target="96">0</span>–<span class="hero-counter" data-target="99">0</span>% Methane Purity</span>
                         </div>
                    
                     </div>
